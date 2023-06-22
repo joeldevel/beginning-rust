8b9b4bbf68f9a477fd9aceff0c8f33f4a19139c4
joeldevel <christianjoel.developer@gmail.com>
2023-06-21 22:11:24 -0300

    first part of the game done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,22 @@
+use std::io; // used for getting user input from terminal and output
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!"); // println! is a macro
+    
+    println!("Please input your guess.");
+    
+    // let creates a variable. mut means its value can change
+    // variables are inmutable by default
+    // `String::new` returns a new instance of a String
+    // `::` means `new` is a function associated to the type
+    let mut guess = String::new();
+
+    // references are inmutable by default too
+    // read_line returns a `Result`, used for error check
+    // if error then print the message
+    io::stdin()
+        .read_line(&mut guess) // & means a reference
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}"); // placeholder
 }
